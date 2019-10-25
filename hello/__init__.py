f934b494710d32f539f815f1db0af39f6a48a716
penguin138 <fridaythethirteens@gmail.com>
2019-10-25 19:50:32 +0300

    initial commit

---
+++ hello/__init__.py
@@ -0,0 +1,15 @@
+
+def say(stuff):
+    print(stuff)
+
+
+def say_hello():
+    say("Hello!")
+
+
+def greet(person):
+    say("Hello, {}!".format(person))
+
+
+def say_goodbye():
+    say("Bye!")
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+  name="test_hello",
+  version="1.0.0",
+  license="Apache License Version 2.0",
+  author="Anastasiia Torunova",
+  python_requires=">=3.5",
+  description='Package for testing Travis CI intergation',
+  packages=find_packages(),
+)
